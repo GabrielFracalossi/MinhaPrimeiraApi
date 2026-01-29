8ab8fa7a8c02de337d80d307c68051888ceae713
Gabriel Fracalossi <g4brielfnazario@gmail.com>
2026-01-29 15:34:40 -0300

    feat: Create controller

---
+++ Controllers/ExemploController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaPrimeiraApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExemploController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Minha primeira API está funcionando!");
+        }
+    }
+}
