5114df2a6798cb3c5699dd8fb183c556bb9f4013
Gabriel Fracalossi <g4brielfnazario@gmail.com>
2026-01-30 11:23:50 -0300

    feat: Create controller 'ProdutosController'

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaPrimeiraApi.Models;
+
+namespace MinhaPrimeiraApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private static readonly List<Produto> Produtos = new()
+        {
+            new Produto { Id = 1, Nome = "Teclado", Preco = 150 },
+            new Produto { Id = 2, Nome = "Mouse", Preco = 80 }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Produtos);
+        }
+    }
+}
