8d5d1b7e9a61be38af054f17583cd4e0662759f6
Gabriel Fracalossi <g4brielfnazario@gmail.com>
2026-02-18 11:22:01 -0300

    feat: Create produto service and interface

---
+++ Services/IProdutoService.cs
@@ -0,0 +1,11 @@
+﻿using MinhaPrimeiraApi.Models;
+
+namespace MinhaPrimeiraApi.Services
+{
+    public interface IProdutoService
+    {
+        IEnumerable<Produto> ObterTodos();
+        Produto? ObterPorId(int id);
+        void Adicionar(Produto produto);
+    }
+}
---
+++ Services/ProdutoService.cs
@@ -0,0 +1,28 @@
+﻿using MinhaPrimeiraApi.Models;
+
+namespace MinhaPrimeiraApi.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        private static readonly List<Produto> Produtos = new()
+        {
+          new Produto { Id = 1, Nome = "Teclado", Preco = 150 },
+          new Produto { Id = 2, Nome = "Mouse", Preco = 80 }
+        };
+
+        public IEnumerable<Produto> ObterTodos()
+        {
+            return Produtos;
+        }
+
+        public Produto? ObterPorId(int id)
+        {
+            return Produtos.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            Produtos.Add(produto);
+        }
+    }
+}
