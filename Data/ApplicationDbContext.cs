86c998377ff05030ffa935ef1673f690790eab89
Gabriel Fracalossi <g4brielfnazario@gmail.com>
2026-03-05 22:27:47 -0300

    feat: Create application DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaPrimeiraApi.Models;
+
+namespace MinhaPrimeiraApi.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
