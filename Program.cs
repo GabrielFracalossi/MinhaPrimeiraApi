10cf184cdbc6bc7995c87ab9b008bd3be514a059
Gabriel Fracalossi <g4brielfnazario@gmail.com>
2026-03-11 23:22:12 -0300

    fix: Adjusted db connection

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add context to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container (traditional controllers).
 builder.Services.AddControllers();
